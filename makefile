cfb40dc1f9e3baf7bdc611df8ad4edfbfce65569
Oussama Elhassouni <oelhasso@c1r7p5.1337.ma>
2025-04-11 22:51:36 +0100

    save this

---
+++ makefile
@@ -0,0 +1,8 @@
+NAME = gnl
+
+CC = cc
+
+SRC = main.c
+
+all:
+	${CC} ${SRC} -o ${NAME}
---
+++ test.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int n;
+    int index;
+    scanf("%d", &n);
+    char line[n + 1];
+    scanf("%[^\n]", line);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &index);
+    }
+    int i = 1;
+    int j;
+    int pos;
+    while ()
+    {
+        j = 1;
+        pos = 1;
+        while (pos <= n)
+        {
+                if (pos == )
+        }
+    }
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("ordered\n");
+
+    return 0;
+}
